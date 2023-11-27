ee35fe1dc0755bef107a4c51acab938ce00bdada
Konrad Gawda <k.gawda@alx.pl>
2023-11-27 17:02:28 +0100

    Challenge tasks 1 and 2

--- protocols/http_protocol.py
+++ protocols/http_protocol.py
@@ -12,5 +12,21 @@ def main():
     # response.raise_for_status()  # requests.exceptions.HTTPError: 404 Client Error: Not Found for url: https://www.onet.pl/asdasdsaa
     print(response.headers)
 
+    # # POST
+    # response = requests.post("http://...", data='tutaj dane tekstowe')
+    # print(response.status_code)
+    # print(response.text)
+
+def challenge():
+    # response = requests.get("http://warsztat.mywire.org/Konrad/1/data")
+    # print(response.text)
+
+    response = requests.get("http://warsztat.mywire.org/Konrad/2/data")
+    data = response.text
+    print(data)
+    response = requests.post("http://warsztat.mywire.org/Konrad/2/solution", data=data)
+    print(response.text)
+
 if __name__ == "__main__":
     main()
+    # challenge()  # uncomment to run challenge!
