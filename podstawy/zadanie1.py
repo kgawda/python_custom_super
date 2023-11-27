53f740dcb4ebe8fd19bc7766a964b2aea519de5a
Konrad Gawda <k.gawda@alx.pl>
2023-11-27 14:08:07 +0100

    Zadanie1

---
+++ podstawy/zadanie1.py
@@ -0,0 +1,9 @@
+"""
+Zadanie: napisać skrypt, który odczytuje csv_example2.csv
+i dla każdej jego linijki drukuje na konsoli user_label i MO.
+Np.:
+34_XYZ1234A_101L18_O_ABC EUtranCellFDD=0
+34_XYZ1234A_201L18_O_ABC EUtranCellFDD=1
+34_XYZ1234A_301L18_O_ABC EUtranCellFDD=2
+...
+"""
