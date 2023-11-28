8e69f28864dfe9ee39547d9e4d872c5c9eac88fa
Konrad Gawda <k.gawda@alx.pl>
2023-11-28 17:03:17 +0100

    Pandas groupby itp

--- data_analysis/basic_analysis.py
+++ data_analysis/basic_analysis.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def main():
+def main_csv():
     df = pd.read_csv("csv_example1.csv", delimiter=";")
     print(df.iloc[0])  # pierwszy rekord
     # print(df["userLabel"])  # wyciągamy daną kolumnę
@@ -12,6 +12,23 @@ def main():
     print(df[df["tiltPercent"] > 30]["userLabel"].values)
     print(df[df["tiltPercent"] > 30]["userLabel"].unique())  # bez powtarzających się wartości
 
+    df["radioBand"] = df["userLabel"].str[-3:]
+
+    print(df[["radioBand", "electricalAntennaTilt"]].groupby("radioBand").mean())
+
+
+def main_txt():
+    df = pd.read_fwf(
+        "txt_example1.txt",
+        skiprows=7,
+        skipfooter=4,
+        widths=[15, 35, 21, 32, 24],
+        parse_dates=["Cell latest setup time"]
+    )
+    print(list(df))
+    print(df.dtypes)
+    print(df.describe())
+    print(df.iloc[df["Cell latest setup time"].idxmax()]["Cell Name"])
 
 if __name__ == "__main__":
-    main()
+    main_csv()
