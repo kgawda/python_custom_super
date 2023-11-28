f3b83c973561938eec9dbc6b7e4aec6076c57453
Konrad Gawda <k.gawda@alx.pl>
2023-11-28 15:33:34 +0100

    SSH parsing

--- protocols/ssh_protocol.py
+++ protocols/ssh_protocol.py
@@ -4,9 +4,14 @@ def main():
     connection = fabric.Connection(
         "warsztat.mywire.org",
         user="student",
-        connect_kwargs={"password": "_______"}
+        connect_kwargs={"password": "____"}
     )
-    connection.run("ls -al")
+    result = connection.run("free", hide=True)
+    command_output = result.stdout
+    available_mem = int(command_output.splitlines()[1].split()[-1])
+    print("Available memory", round(available_mem/1024), "MiB")
+
+    connection.close()
 
 
 
