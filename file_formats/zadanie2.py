0c740405711104ca64150c89e655b5daed9a47fb
Konrad Gawda <k.gawda@alx.pl>
2023-11-28 10:14:54 +0100

    Zadanie2 - podpowiedzi

--- file_formats/zadanie2.py
+++ file_formats/zadanie2.py
@@ -4,8 +4,14 @@ Wydrukować po jednym wierszu dla każdej stacji z json_example.json.
 Wiersz powinien zawierać: id, short_name, insert_date
 
 Poziom 2:
-Powyższe wydrukować w kolejności, posortowane po rosnącym id
+Powyższe wydrukować w kolejności, posortowane po rosnącym id.
+Podpowiedź:
+    najpierw napisz funkcję get_id, wyciągającą id dla danego wiersza.
+    Potem użyj pętli: for ... in sorted(..., key=get_id)
 
 Poziom 3:
 Zamiast insert_date wydrukować czas względny, np. "123 dni temu"
+Podpowiedź:
+    na początku pliku dodaj: import datetime
+    Następnie użyj datetime.datetime.strptime(...)
 """
