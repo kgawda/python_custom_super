e0e9cb576e1f9c9d24898b30e28912844d5fca0a
Konrad Gawda <k.gawda@alx.pl>
2023-11-28 15:39:04 +0100

    SSH scp

--- protocols/ssh_protocol.py
+++ protocols/ssh_protocol.py
@@ -6,10 +6,18 @@ def main():
         user="student",
         connect_kwargs={"password": "____"}
     )
+
+    # Wywołanie i analiza komendy
     result = connection.run("free", hide=True)
     command_output = result.stdout
     available_mem = int(command_output.splitlines()[1].split()[-1])
     print("Available memory", round(available_mem/1024), "MiB")
+
+    # Pobieranie pliku przez scp:
+    connection.get(
+        "/home/student/data.txt",
+        # local="downloaded.txt"
+    )
 
     connection.close()
 
