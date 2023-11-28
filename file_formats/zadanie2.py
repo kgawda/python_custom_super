ffedb0cc008a075cc4aff62146bf7f4470da8951
Konrad Gawda <k.gawda@alx.pl>
2023-11-28 10:08:53 +0100

    Zadanie2

---
+++ file_formats/zadanie2.py
@@ -0,0 +1,11 @@
+"""
+Zadanie, poziom 1:
+Wydrukować po jednym wierszu dla każdej stacji z json_example.json.
+Wiersz powinien zawierać: id, short_name, insert_date
+
+Poziom 2:
+Powyższe wydrukować w kolejności, posortowane po rosnącym id
+
+Poziom 3:
+Zamiast insert_date wydrukować czas względny, np. "123 dni temu"
+"""
