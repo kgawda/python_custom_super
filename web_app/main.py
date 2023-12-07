c48dbf9b2593938eead058f880bf80cbefa69f5d
Konrad Gawda <k.gawda@alx.pl>
2023-12-07 16:36:23 +0100

    extending templates and dynamic content

--- web_app/main.py
+++ web_app/main.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,5 +8,10 @@ app = Flask(__name__)
 def hello():
     return render_template("index.html")
 
+@app.route("/diece")
+def roll_a_diece():
+    result = random.randrange(1, 7)
+    return render_template("result.html", result=result)
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True)  # use_reloader=False
