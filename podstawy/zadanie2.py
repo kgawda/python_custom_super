1a59723914ac010c91bc204ccc555f42abce7b61
Konrad Gawda <k.gawda@alx.pl>
2023-12-07 15:28:23 +0100

    Zadanie z re

--- podstawy/zadanie2.py
+++ podstawy/zadanie2.py
@@ -5,9 +5,14 @@ def main():
         s = f.read()
 
     # Zad., cz1. Zanlazeźć wszystkie Cell Name w pliku
+    #   Np.: 34_XYZ1234A_101L18_O_ABC
     # Zad., cz2. Wyciągnąć za pomocą groups poszczególne elementy z Cell Name
-    for cell in re.findall(r"...", s):
-        print(cell)
+
+    # for cell in re.findall(r"(\d{2})_([A-Z0-9]{8})_(\d{3})([A-Z]\d{2})\w*", s):
+    #    print(cell)
+
+    for region, cell_id, sector, tech in re.findall(r"(\d{2})_([A-Z0-9]{8})_(\d{3})([A-Z]\d{2})\w*", s):
+        print(f"Region {region}: {cell_id}/{sector} radio {tech}")
 
 if __name__ == "__main__":
     main()
