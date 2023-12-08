a0aa47581d1bfd59f56ee00a07c53f9da59d1ac0
Konrad Gawda <k.gawda@alx.pl>
2023-12-08 10:28:05 +0100

    More sql

--- protocols/sql_databases.py
+++ protocols/sql_databases.py
@@ -48,8 +48,17 @@ def query_two_dbs(mssql_query, mysql_query):
     if mysql_query:
         print("=== MySQL ===")
         test_mysql(mysql_query)
+
 def main():
-    query_two_dbs('SELECT name FROM master.sys.databases', "SHOW DATABASES;")
+    # query_two_dbs('SELECT name FROM master.sys.databases', "SHOW DATABASES;")
+    # query_two_dbs('', "SELECT 4+4")  # --> 8  # na MS SQL nie zadziała z AsDict
+    # query_two_dbs('', "SELECT 3, 4, 5")
+    # query_two_dbs('SELECT * FROM zawodnicy', "SELECT * FROM zawodnicy")
+    # query_two_dbs('SELECT imie, nazwisko FROM zawodnicy', "SELECT imie, nazwisko FROM zawodnicy")
+    query_two_dbs(
+        '',  # 'SELECT nazwisko, waga / POWER(wzrost/100, 2) AS bmi FROM zawodnicy',
+        "SELECT nazwisko, waga / POW(wzrost/100, 2) AS bmi FROM zawodnicy"
+    )
 
 if __name__ == "__main__":
     main()
